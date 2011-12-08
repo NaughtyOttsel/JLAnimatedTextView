639c9f4ab042caf80bfd99f579c0daa219b79eb8
NaughtyOttsel <naughtyottsel@yahoo.co.uk>
2011-12-08 21:36:37 +0000

    Initial Commit

---
+++ JLAnimatedTextView/JLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JLAppDelegate.h
+//  JLAnimatedTextView
+//
+//  Created by James Lee on 08/12/2011.
+//  Copyright (c) 2011 Touch Device Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JLAnimatedTextView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JLAnimatedTextView
+//
+//  Created by James Lee on 08/12/2011.
+//  Copyright (c) 2011 Touch Device Company. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JLAppDelegate class]));
+    }
+}
